924799a6d9efef77745ecba63aa6778b83d5886c
kanguru899 <kanguru899@gmail.com>
2017-01-23 11:21:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://kanguru:kanguru@123@ds127389.mlab.com:27389/kanguru',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || '3kSferTdEhz6Z92fkylrjFetTP5w89frfAVhycuT',
+  masterKey: process.env.MASTER_KEY || 'LXv1jovs65RIgaFnFK3UEn2LkoTwCNJvbQfeMDfY', //Add your master key here. Keep it secret!
+  cleinrKey: process.env.CLIENT_KEY || '', //Add your client key here. Keep it secret!
+  fileKey: process.env.FILE_KEY || '716e1338-acde-4376-a6bb-8d51d735d74a', // Add your file key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://kanguru899.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
